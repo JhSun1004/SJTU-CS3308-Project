10b1503285fe6e3b4dd225ed149053bbcf339060
JhSun1004 <123916626+JhSun1004@users.noreply.github.com>
2024-06-06 15:28:50 +0800

    Update task1.py

--- task1/task1.py
+++ task1/task1.py
@@ -1,41 +1,49 @@
-from GNN import GCN
+from GCN import GCN
 import torch
-from dataset import *
+import numpy as np
 from matplotlib import pyplot as plt
-import tqdm
-from torch_geometric.data import Data
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+from tqdm import *
+from torch_geometric.loader import DataLoader
+from dataset import load_data
+device = torch.device('cuda:2' if torch.cuda.is_available() else 'cpu')
 MODEL_PATH = './model/'
 class AIGTrain():
     def __init__(self):
-        self.model = GCN().to(device)
-        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=0.01, weight_decay=5e-4)
+        self.model = GCN(2).to(device)
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=0.1)
+        self.lr_scheduler = torch.optim.lr_scheduler.StepLR(self.optimizer, step_size=20, gamma=0.5)
         self.loss = torch.nn.MSELoss()
-        self.batch_size = 32
-        self.trainset = torch.load('train_set.pt')
-        self.testset = torch.load('test_set.pt')
+        self.batch_size = 16
+        self.trainset = torch.load('split_train_data.pt')
+        self.testset = torch.load('split_test_data.pt')
     
     def train(self):
-        trainloader = torch.utils.data.DataLoader(self.trainset, batch_size=self.batch_size, shuffle=True)
-        self.model.train()
-        loss = int('inf')
-        for i in tqdm(range(100)):
+        trainloader = DataLoader(self.trainset, batch_size=self.batch_size, shuffle=True)
+        min_loss = 100
+        print(self.model)
+        print('Start training')
+        for i in range(1000):
+            self.model.train()
             for data in trainloader:
-                data = Data(x = torch.cat([data['node_type'], data['num_inverted_predecessors']], dim=1), edge_index = data['edge_index'], y = data['y'])
                 data = data.to(device)
                 self.optimizer.zero_grad()
                 out = self.model(data)
                 loss = self.loss(out, data.y)
                 loss.backward()
                 self.optimizer.step()
+                self.lr_scheduler.step()
             new_loss = self.test()
-            if new_loss < loss:
-                loss = new_loss
+            print('\nEpoch: {}, Loss: {}'.format(i, new_loss))
+            if new_loss < min_loss:
+                min_loss = new_loss.clone()
                 torch.save(self.model.state_dict(), MODEL_PATH + str(i) + '.pth')
+                print('Model saved')
+        # self.final_test()    
 
     def test(self):
         self.model.eval()
-        testloader = torch.utils.data.DataLoader(self.testset, batch_size=self.batch_size, shuffle=True)    
+        testloader = DataLoader(self.testset, batch_size=self.batch_size, shuffle=True)
+        # testloader = torch.utils.data.DataLoader(self.testset, batch_size=self.batch_size, shuffle=True)    
         # plt.scatter(np.array(y), np.array(out))
         # plt.plot(np.array([0, 1]), np.array([0, 1]))
         # plt.xlabel('y')
@@ -44,12 +52,28 @@ class AIGTrain():
         # plt.savefig('./graph/prediction.png')
         loss = 0
         for data in testloader:
-            data = Data(x = torch.cat([data['node_type'], data['num_inverted_predecessors']], dim=1), edge_index = data['edge_index'], y = data['y'])
             data = data.to(device)
             out = self.model(data)
             loss += self.loss(out, data.y)
         return loss
     
+    def final_test(self):
+        self.model.eval()
+        y = []
+        out = []
+        testloader = DataLoader(self.testset, batch_size=self.batch_size, shuffle=True)   
+        for data in testloader:
+            data = data.to(device)
+            new_out = self.model(data, data.batch)
+            out.append(new_out)
+            y.append(data.y)
+        plt.scatter(np.array(y), np.array(out))
+        plt.plot(np.array([0, 1]), np.array([0, 1]))
+        plt.xlabel('y')
+        plt.ylabel('out')
+        plt.title('Prediction of AIG evaluation')
+        plt.savefig('./prediction.png')
+
 if __name__ == '__main__':
     aig = AIGTrain()
     aig.train()
