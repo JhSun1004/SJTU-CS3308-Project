fe87cf1e8b25a5149f4dd5dd2a7b98c97dc4a38c
JhSun1004 <123916626+JhSun1004@users.noreply.github.com>
2024-06-08 19:25:52 +0800

    Update GNN.py

--- task1/GNN.py
+++ task1/GNN.py
@@ -1,32 +1,23 @@
 import torch
 import torch.nn.functional as F
 from torch_geometric.nn import GCNConv, global_mean_pool
-from torch_geometric.datasets import TUDataset
-from torch_geometric.data import DataLoader
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    
 class GCN(torch.nn.Module):
-    def __init__(self, num_features):
+    def __init__(self):
         super(GCN, self).__init__()
-        self.conv1 = GCNConv(num_features, 32)
-        self.conv2 = GCNConv(32, 64)
-        self.fc = torch.nn.Linear(64, 8)
-        self.fc2 = torch.nn.Linear(8, 1)
+        self.conv1 = GCNConv(2, 16)  
+        self.conv2 = GCNConv(16, 16)  
+        self.lin = torch.nn.Linear(16, 1)
 
     def forward(self, data):
         x, edge_index, batch = data.x, data.edge_index, data.batch
         x = self.conv1(x, edge_index)
-        x = torch.relu(x)
-        # x = torch.dropout(x, p=0.5, train=self.training)
-
+        x = F.relu(x)
         x = self.conv2(x, edge_index)
-        x = torch.relu(x)
-        # x = torch.dropout(x, p=0.5, train=self.training)
-
-        x = global_mean_pool(x, batch) # Add global mean pooling
-
-        x = self.fc(x)
-        x = torch.relu(x)
-
-        x = self.fc2(x)
-        x = x.reshape(-1)
+        x = F.relu(x)
+        # 2. Readout layer
+        x = global_mean_pool(x, batch)  # [batch_size, hidden_channels]
+        # 3. Apply a final classifier
+        x = self.lin(x)
         return x
