1dcfb46e356ff5984495b5cc063442a7825a64c0
JhSun1004 <123916626+JhSun1004@users.noreply.github.com>
2024-06-08 19:26:26 +0800

    Update data_process.py

--- task1/data_process.py
+++ task1/data_process.py
@@ -9,8 +9,7 @@ for i, data in enumerate(dataset):
     x = x.reshape(2, -1).T
     x = torch.tensor(x, dtype=torch.float32)
     edge_index = data['edge_index']
-    batch = torch.zeros((x.size(0),), dtype=torch.long)
-    new_data = Data(x = x, edge_index = edge_index, y = data['y'], batch = batch)
+    new_data = Data(x = x, edge_index = edge_index, y = data['y'])
     processed_data.append(new_data)
 train_data, test_data = train_test_split(processed_data, test_size=0.2, shuffle=True, random_state=42)
 torch.save(train_data, 'split_train_data.pt')
